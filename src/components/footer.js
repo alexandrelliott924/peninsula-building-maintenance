3a73f4f418d27d8e7e1d1519bc2b8bbb4ef83bdc
Alex Elliott <alexandre.elliott@gmail.com>
2026-06-01 14:15:24 +0800

    Added 'tap to call' feature in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ function Footer() {
         <div className="footer">
             <img src={logo} alt="PBM logo" className="PBM-footer-logo" />
             <p className="footer-text"> <span className="enveloppe">✉</span> admin@peninsula-bm.com.au</p>
-            <p className="footer-text"><span className="phone">☎</span> 0401 443 548</p>
+            <p className="footer-text"><span className="phone">☎</span> <a href="tel:0401443548" className="phone-link">0401 443 548</a></p>
             <p className="footer-text">ABN: 526 725 932 52</p>
         </div>
     )
