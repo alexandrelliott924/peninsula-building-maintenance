afb2753b94655c251e7f370991acf63af8bcf36b
Alex Elliott <alexandre.elliott@gmail.com>
2026-06-15 20:00:33 +0800

    undo last commit

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.post('/api/contact', async (req, res) => {
 
     // Email signature with logo and company details
     const emailSignature = `
-      <hr style="border: none; border-top: 1px solid #ddd; margin: 0, 0, 5px 0;">
+      <hr style="border: none; border-top: 1px solid #ddd; margin: 5px 0;">
       <div style="margin-top: 5px; margin-bottom: 5px; margin-left: 0px">
         <img src="https://github.com/alexandrelliott924/peninsula-building-maintenance/blob/dev/frontend/src/assets/logos/PBM-logo-white-background.jpeg?raw=true" alt="Peninsula Building Maintenance" style="width: 50%; height: auto; max-width: 220px;">
       </div>
